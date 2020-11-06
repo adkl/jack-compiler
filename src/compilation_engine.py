773ff5f60abb41983818b15d0abc06fd67d676ce
Adnan Alibegovic <aalibegovi2@etf.unsa.ba>
2020-11-06 23:53:42 +0100

    Compilation engine WIP - built class and varDec parsing

--- src/compilation_engine.py
+++ src/compilation_engine.py
@@ -11,45 +11,20 @@ class CompilationEngine:
         self.file_paths_to_compile = self.__get_paths_to_compile(path)
         self.is_directory = Path(path).is_dir()
 
+        self.tokenizer = None
+
     def compile(self, export_xml: bool = True):
         for file_path in self.file_paths_to_compile:
             self.compile_file(file_path, export_xml)
 
     def compile_file(self, file_path: str, export_xml: bool = True):
-        tokenizer = Tokenizer(file_path)
+        self.tokenizer = Tokenizer(file_path)
 
-        data = ET.Element('tokens')
-        data.tail = '\n'
+        self.__compile_class()
 
-        while tokenizer.has_more_tokens():
-            tokenizer.advance()
 
-            token_element = ET.SubElement(data, tokenizer.token_type.value)
-
-            token = None
-            if tokenizer.token_type == TokenType.symbol:
-                token = tokenizer.symbol
-
-            if tokenizer.token_type == TokenType.identifier:
-                token = tokenizer.identifier
-
-            if tokenizer.token_type == TokenType.int_constant:
-                token = tokenizer.int_val
-
-            if tokenizer.token_type == TokenType.string_constant:
-                token = tokenizer.string_val
-
-            if tokenizer.token_type == TokenType.keyword:
-                token = tokenizer.keyword
-
-            token_element.text = f' {token} '
-            token_element.tail = '\n'
-
-        if export_xml:
-            tokens = ET.tostring(data).decode('UTF-8')
-
-            xml = open(str(Path(file_path).with_suffix('.xml')), 'w')
-            xml.write(tokens)
+    def __compile_var_dec(self):
+        pass
 
     def __get_paths_to_compile(self, path: str) -> List[str]:
         os_path = Path(path)
@@ -58,3 +33,83 @@ class CompilationEngine:
             return [str(entry) for entry in os_path.iterdir() if entry.suffix == '.jack']
 
         return [path]
+
+    def __compile_class(self):
+        tokenizer = self.tokenizer
+
+        tokenizer.advance()
+        if tokenizer.token_type != TokenType.keyword or tokenizer.keyword != 'class':
+            raise SyntaxError("The code must be wrapped inside a class.")
+
+        tokenizer.advance()
+        if tokenizer.token_type != TokenType.identifier:
+            raise SyntaxError("Class name expected.")
+
+        tokenizer.advance()
+        self.__check_is_left_curly_brace(tokenizer)
+
+        self.__compile_class_var_dec()
+
+        self.__compile_subroutine_dec()
+
+        self.__check_is_right_curly_brace(tokenizer)
+
+    def __compile_class_var_dec(self):
+        tokenizer = self.tokenizer
+
+        tokenizer.advance()
+        is_var = tokenizer.token_type == TokenType.keyword and tokenizer.keyword == 'var'
+
+        if not is_var:
+            return
+
+        while is_var:
+            tokenizer.advance()
+            if not self.__is_type(tokenizer):
+                raise SyntaxError("Invalid type in class variable declaration.")
+
+            tokenizer.advance()
+            if not tokenizer.token_type != TokenType.identifier:
+                raise SyntaxError("Invalid identifier in class variable declaration.")
+
+            tokenizer.advance()
+            is_var = tokenizer.token_type == TokenType.keyword and tokenizer.keyword == 'var'
+
+
+    def __compile_subroutine_dec(self):
+        tokenizer = self.tokenizer
+
+        is_subroutine = tokenizer.token_type != TokenType.keyword \
+                        or tokenizer.keyword not in ['constructor', 'function', 'method']
+
+        if not is_subroutine:
+            return
+
+        while is_subroutine:
+            tokenizer.advance()
+            if not self.__is_type(tokenizer) or not tokenizer.keyword == 'void':
+                raise SyntaxError("Subroutine name should be followed by type.")
+
+            tokenizer.advance()
+            if tokenizer.token_type != TokenType.identifier:
+                raise SyntaxError(f"Invalid identifier for subroutine {tokenizer.identifier}.")
+
+            tokenizer.advance()
+            self.__check_is_left_curly_brace(tokenizer)
+            self.__check_is_right_curly_brace(tokenizer)
+
+            is_subroutine = tokenizer.token_type != TokenType.keyword \
+                            or tokenizer.keyword not in ['constructor', 'function', 'method']
+
+
+    def __is_type(self, tokenizer):
+        return tokenizer.token_type in [TokenType.keyword, TokenType.identifier] \
+               or tokenizer.keyword in ['int', 'char', 'boolean']
+
+    def __check_is_left_curly_brace(self, tokenizer):
+        if tokenizer.token_type != TokenType.symbol or tokenizer.symbol != '{':
+            raise SyntaxError(f"Expected {{.")
+
+    def __check_is_right_curly_brace(self, tokenizer):
+        if tokenizer.token_type != TokenType.symbol or tokenizer.symbol != '}':
+            raise SyntaxError(f"Expected }}.")
